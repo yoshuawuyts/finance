13609108d0a066d788afd197fbc08f5bda133ee9
Yoshua Wuyts <i@yoshuawuyts.com>
2015-01-25 22:09:16 +0100

    sum: add multi-output

--- sum.js
+++ sum.js
@@ -1,15 +1,31 @@
 
+const minimist = require('minimist')
 const csv = require('csv-parser')
+const pad = require('pad-number')
 const assert = require('assert')
 const fs = require('fs')
 
-var res = 0
+const argv = minimist(process.argv.slice(2))
 
-fs.createReadStream('./2015/01_spendings.csv')
-  .pipe(csv())
-  .on('data', function(data) {
-    if (data.value) res += parseInt(data.value)
-  })
-  .on('end', function() {
-    console.log('sum: ' + res)
-  })
+const month = pad(argv._[0], 2)
+const spendings = query(month, 'spendings')
+const income = query(month, 'income')
+
+calc('income', income)
+calc('spendings', spendings)
+
+function calc(type, file) {
+  var res = 0
+  fs.createReadStream(file)
+    .pipe(csv())
+    .on('data', function(data) {
+      if (data.value) res += parseInt(data.value)
+    })
+    .on('end', function() {
+      console.log(type + ': ' + res)
+    })
+}
+
+function query(month, type) {
+  return './2015/' + month + '_' + type + '.csv'
+}
